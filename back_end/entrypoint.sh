eeed283a004194ed0ab97f042c1986cf508b0403
synvoret <synvoret@gmail.com>
2025-05-14 21:17:50 +0200

    Docker_1

---
+++ back_end/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "👉 Migration"
+python manage.py migrate --noinput
+
+echo "👉 Create Super User"
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@admin.com", "admin")
+EOF
+
+echo "👉 Server start"
+exec python manage.py runserver 127.0.0.1:8000
